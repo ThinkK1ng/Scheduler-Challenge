41ee1c1b5414441bee3c02d7a12afe0e9b8e055e
ThinkK1ng <cla.givens@gmail.com>
2022-11-14 16:58:29 -0600

    started JavaScript

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// This Display's today's day and date.
+var currentDate = moment().format('dddd,MMM Do YYYY')
+$("currentDay").html(currentDate);
+
+$(document).ready(function () {
+    //saveBtn click listener here
+    $(".saveBtn").on("click", funtion() {
+        var text=
+    })
+})
